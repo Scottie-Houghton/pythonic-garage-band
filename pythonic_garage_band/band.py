f88ad6e600f9e37b5cfb67fa08a1dc52636bcad4
Scottie <scott.c.houghton@gmail.com>
2022-07-21 23:31:43 -0700

    Created a requirements file and installed requirements. Set up test directory. Added a pythonic_garage_band directory and band module. Added classes.

---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,18 @@
+class Band:
+    pass
+
+class Musician:
+    pass
+
+class Guitarist:
+    def __init__(self, name):
+        self.name = name
+
+    def str(self):
+        return f"My name is {self.name} and I play guitar"
+
+class Bassist:
+    pass
+
+class Drummer:
+    pass
